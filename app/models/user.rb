73a7ae46c335c31f11fa8efd61b5dab5835082d1
slavakisel <slava.kisel@flatstack.com>
2016-07-19 11:15:41 +0300

    Remove unnecessary devise modules from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable
+  devise :database_authenticatable, :rememberable
 
   has_many :accounts
   has_many :companies, through: :accounts
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users
 
   resource :registration, only: %i(new create)
   get "sign_up", to: "registrations#new"
