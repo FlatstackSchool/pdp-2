97a6f94f7ad2dcbf64a99e214e6f6b8d4f5ed7aa
slavakisel <slava.kisel@flatstack.com>
2016-07-19 12:22:15 +0300

    Fix auth cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,13 @@ class ApplicationController < ActionController::Base
   responders :flash
   respond_to :html
 
+  before_action :redirect_to_root
+
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
+  end
+
+  def redirect_to_root
+    redirect_to root_url(subdomain: nil) if !user_signed_in? && !request.subdomain.blank?
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,8 @@ class SessionsController < Devise::SessionsController
   def after_sign_in_path_for(user)
     root_url(subdomain: user.companies.first.subdomain)
   end
+
+  def after_sign_out_path_for(user)
+    root_url(subdomain: nil)
+  end
 end
